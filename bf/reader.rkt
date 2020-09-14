d9341576a0440bd2dca35af95e61f86fcb12d079
Harman Mahal <harmanmahal@gmail.com>
2020-09-13 23:00:13 -0700

    Clean up and packaging

--- bf/reader.rkt
+++ bf/reader.rkt
@@ -3,7 +3,7 @@
 
 (define (read-syntax path port)
   (define parse-tree (parse path (make-tokenizer port)))
-  (define module-datum `(module bf-mod "funexpander.rkt" ,parse-tree))
+  (define module-datum `(module bf-mod bf/expander ,parse-tree))
   (datum->syntax #f module-datum))
 (provide read-syntax)
 
